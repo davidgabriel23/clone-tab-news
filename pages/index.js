ef86bdeae35ad614c094ab93da447d70870949e7
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-03-01 00:15:51 +0000

    Base do projeto

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    
+    
+    return <h1>Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa.</h1>
+
+}
+
+export default Home;
