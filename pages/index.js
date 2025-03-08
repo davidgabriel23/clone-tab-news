06431232d84a21c0a4b9ffcec9939441d8d1abe9
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-03-08 00:20:11 +0000

    mudança na frase h1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     
     
-    return <h1>Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa.</h1>
+    return <h1>Para ter sucesso, você deve eliminar dúvidas, aceitar desafios e rejeitar qualquer negatividade externa.:)</h1>
 
 }
 
