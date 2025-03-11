b701605c980f5cab7834c697c6e05b2acc8f4d0a
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-03-11 00:01:50 +0000

    atualizando o H1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     
     
-    return <h1>Se você não vivenciar o fracasso, não saberá reconhecer quando o sucesso realmente chegar.</h1>
+    return <h1>Comece onde você está, use o que você tem e faça o que você pode.</h1>
 
 }
 
