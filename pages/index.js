d829dc1ff8937c3c602413b22b90e69b0dbfa1eb
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-03-16 12:35:11 +0000

    att da frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     
     
-    return <h1>Comece onde você está, use o que você tem e faça o que você pode.</h1>
+    return <h1>Quando estiver tudo muito bagunçado na sua vida, tente começar arrumando algo pequeno. Olhe para o seu quarto e organize algo nele. Depois, organize mais alguma coisa. Quando você for ver, já estará voltando a organizar a sua vida.</h1>
 
 }
 
