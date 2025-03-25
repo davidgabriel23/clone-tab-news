b7fbfc6442efa51c41d39a9c8757acb77f8d6b27
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-03-25 11:05:15 +0000

    att frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     
     
-    return <h1>Você é uma pessoa incrível, cuja presença ilumina qualquer ambiente e faz tudo ao seu redor mais especial.</h1>
+    return <h1>Mesmo nos dias difíceis, lembre-se de que você tem uma luz dentro de si que nunca se apaga. Vai ficar tudo bem, e eu estou aqui ao seu lado.</h1>
 
 }
 
