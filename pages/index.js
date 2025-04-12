2794bb27980971b564cc44dd893a1313f3c85d6a
David Gabriel <120142381+davidgabriel23@users.noreply.github.com>
2025-04-12 15:58:52 +0000

    att frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
   return (
-    <h1>Comece onde você está, use o que você tem e faça o que você pode.</h1>
+    <h1>Sei que as coisas não têm sido fáceis ultimamente. Sei do seu cansaço, da sua tristeza e da vontade de jogar tudo pro alto. Mas muitas vezes é quando menos esperamos que o melhor simplesmente acontece e tudo passa a fazer sentido! Você vai ver como tudo o que está acontecendo agora vai se encaixar mais pra frente e que todas essas coisas precisavam mesmo acontecer para os seus objetivos se concretizarem! Nunca se esqueça de que estou do seu lado pra te dar todo o apoio de que precisar!
+
+    </h1>
   );
 }
 
